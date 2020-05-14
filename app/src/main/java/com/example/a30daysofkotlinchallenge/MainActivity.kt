26e69c461e1fe50de6cda0750b06aa8f0b3b31cc
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-15 01:04:45 +0530

    Play with linear layout https://codelabs.developers.google.com/codelabs/kotlin-android-training-linear-layout/index.html#9

--- app/src/main/java/com/example/a30daysofkotlinchallenge/MainActivity.kt
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/MainActivity.kt
@@ -1,39 +1,15 @@
 package com.example.a30daysofkotlinchallenge
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage1 : ImageView
-    lateinit var diceImage2 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        diceImage1  = findViewById(R.id.dice_image)
-        diceImage2  = findViewById(R.id.dice_image2)
-        rollButton.setOnClickListener { rollDice() }
-    }
-    private fun rollDice(){
-        
-        diceImage1.setImageResource(getRandomDiceImage())
-        diceImage2.setImageResource(getRandomDiceImage())
+
     }
 
-    private fun getRandomDiceImage(): Int{
-        val randomInt = (1..6).random()
-        val drawelResource = when(randomInt){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        return drawelResource;
-    }
 
 }
