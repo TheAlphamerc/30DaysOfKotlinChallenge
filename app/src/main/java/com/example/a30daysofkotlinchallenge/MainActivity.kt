83e797aaaec96061a8753470287130602982b50a
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-15 23:46:46 +0530

    Constraint layout using the Layout Editor part 1 https://codelabs.developers.google.com/codelabs/kotlin-android-training-constraint-layout/index.html

--- app/src/main/java/com/example/a30daysofkotlinchallenge/MainActivity.kt
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.a30daysofkotlinchallenge
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
     }
+
+    private fun setListeners() {
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableViews: List<View> = listOf(
+            boxOneText,
+            boxTwoText,
+            boxThreeText,
+            boxFourText,
+            boxFiveText,
+            rootConstraintLayout
+        )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(item) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.RED)
+            else -> view.setBackgroundColor(Color.GREEN)
+        }
+    }
+
 }
