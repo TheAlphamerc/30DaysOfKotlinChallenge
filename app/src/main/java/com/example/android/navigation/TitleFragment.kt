59765a3dc088dbf5bfb42586b2104ebba0940f17
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-16 18:41:00 +0530

    Create a fragment https://codelabs.developers.google.com/codelabs/kotlin-android-training-create-and-add-fragment/index.html

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,31 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+
+}
