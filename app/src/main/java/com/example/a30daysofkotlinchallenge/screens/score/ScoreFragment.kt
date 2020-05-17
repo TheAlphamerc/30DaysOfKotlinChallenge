a3deeaae44251c27c854bce296cc29a0b33febe0
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-17 21:29:51 +0530

    Guess the word starter project setup

---
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/screens/score/ScoreFragment.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.a30daysofkotlinchallenge.screens.score
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.a30daysofkotlinchallenge.R
+import com.example.a30daysofkotlinchallenge.databinding.ScoreFragmentBinding
+
+/**
+ * Fragment where the final score is shown, after the game is over
+ */
+class ScoreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate view and obtain an instance of the binding class.
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.score_fragment,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
