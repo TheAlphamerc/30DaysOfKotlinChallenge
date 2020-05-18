3b083cd1e20821daa55e7821a1bdb21916a4f947
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-18 21:17:15 +0530

    Encapsulate the LiveData

--- app/src/main/java/com/example/a30daysofkotlinchallenge/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/screens/game/GameViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.a30daysofkotlinchallenge.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     // The current word
-    var word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -45,8 +50,8 @@ class GameViewModel : ViewModel() {
     }
 
     init {
-        word.value = ""
-        score.value = 0
+        _word.value = ""
+        _score.value = 0
         resetList()
         nextWord()
         Log.i("GameViewModel", "GameViewModel created!")
@@ -58,19 +63,19 @@ class GameViewModel : ViewModel() {
     fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
 
     }
 
     /** Methods for buttons presses **/
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
