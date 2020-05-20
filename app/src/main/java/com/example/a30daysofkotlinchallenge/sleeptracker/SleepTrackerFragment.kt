805bda3589b7de14d67bb68c1d24a291c165192c
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-20 21:29:48 +0530

    Coroutines and Room https://codelabs.developers.google.com/codelabs/kotlin-android-training-coroutines-and-room/#10

--- app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepTrackerFragment.kt
@@ -22,7 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.a30daysofkotlinchallenge.R
+import com.example.a30daysofkotlinchallenge.database.SleepDatabase
 import com.example.a30daysofkotlinchallenge.databinding.FragmentSleepTrackerBinding
 
 /**
@@ -46,6 +48,20 @@ class SleepTrackerFragment : Fragment() {
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sleep_tracker, container, false
         )
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val sleepTrackerViewModel =
+            ViewModelProviders.of(
+                this, viewModelFactory
+            ).get(SleepTrackerViewModel::class.java)
+
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        binding.lifecycleOwner = this
 
         return binding.root
     }
--- app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepTrackerViewModel.kt
@@ -18,7 +18,13 @@ package com.example.a30daysofkotlinchallenge.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.example.a30daysofkotlinchallenge.database.SleepDatabaseDao
+import com.example.a30daysofkotlinchallenge.database.SleepNight
+import com.example.a30daysofkotlinchallenge.formatNights
+import kotlinx.coroutines.*
+
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -26,5 +32,88 @@ import com.example.a30daysofkotlinchallenge.database.SleepDatabaseDao
 class SleepTrackerViewModel(
     val database: SleepDatabaseDao,
     application: Application
-) : AndroidViewModel(application)
+) : AndroidViewModel(application) {
+
+    private var viewModelJob = Job()
+
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val nights = database.getAllNights()
+
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
+
+    private var tonight = MutableLiveData<SleepNight?>()
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        uiScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+
+        return withContext(Dispatchers.IO) {
+
+            var night = database.getTonight()
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+    }
+
+    fun onStartTracking() {
+        uiScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+
+        }
+    }
+
+    private suspend fun insert(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.insert(night)
+        }
+    }
+
+    fun onStopTracking() {
+        uiScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        withContext(Dispatchers.IO) {
+            database.update(night)
+        }
+    }
+
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
 
