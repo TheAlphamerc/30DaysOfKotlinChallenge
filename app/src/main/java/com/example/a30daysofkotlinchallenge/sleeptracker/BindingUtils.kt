1f4bc99d493641a12a17566ee676a384bcc8a6e6
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-26 22:17:09 +0530

    Create binding adapters DiffUtil and data binding with RecyclerView https://codelabs.developers.google.com/codelabs/kotlin-android-training-diffutil-databinding

---
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/BindingUtils.kt
@@ -0,0 +1,34 @@
+package com.example.a30daysofkotlinchallenge.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.a30daysofkotlinchallenge.R
+import com.example.a30daysofkotlinchallenge.convertDurationToFormatted
+import com.example.a30daysofkotlinchallenge.convertNumericQualityToString
+import com.example.a30daysofkotlinchallenge.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight) {
+    text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight) {
+    text = convertNumericQualityToString(item.sleepQuality, context.resources)
+}
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    setImageResource(
+        when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        }
+    )
+}
--- app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepNightAdapter.kt
@@ -5,9 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.a30daysofkotlinchallenge.R
-import com.example.a30daysofkotlinchallenge.convertDurationToFormatted
-import com.example.a30daysofkotlinchallenge.convertNumericQualityToString
 import com.example.a30daysofkotlinchallenge.database.SleepNight
 import com.example.a30daysofkotlinchallenge.databinding.ListItemSleepNightBinding
 
@@ -32,24 +29,8 @@ class SleepNightAdapter :
         fun bind(
             item: SleepNight
         ) {
-            binding.qualityString.text = item.sleepQuality.toString()
-            val res = itemView.context.resources
-            binding.sleepLength.text =
-                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-            binding.qualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
-            binding.sleepLength.text =
-                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            binding.sleep = item
+            binding.executePendingBindings()
         }
 
         companion object {
