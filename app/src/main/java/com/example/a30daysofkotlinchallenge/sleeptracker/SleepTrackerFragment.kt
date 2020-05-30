011981c4575952e24fa9426a821844ee8d4e9429
Sonu Sharma <sonu.sharma045@gmail.ocm>
2020-05-30 22:45:46 +0530

    07.5: Headers in RecyclerView https://codelabs.developers.google.com/codelabs/kotlin-android-training-headers

--- app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/a30daysofkotlinchallenge/sleeptracker/SleepTrackerFragment.kt
@@ -127,6 +127,14 @@ class SleepTrackerFragment : Fragment() {
         })
 
         val manager = GridLayoutManager(activity, 3, GridLayoutManager.VERTICAL, false)
+
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int) = when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
+
         binding.sleepList.layoutManager = manager
         return binding.root
     }
